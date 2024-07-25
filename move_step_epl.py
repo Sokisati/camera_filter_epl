fb982496ef517eab2d0f5266ec12a88b0c75e378
Sokisati <yagizalpersoy@gmail.com>
2024-07-26 00:45:30 +0300

    bug fix

--- move_step_epl.py
+++ move_step_epl.py
@@ -1,61 +1,45 @@
-from logging import warn
 from gpiozero import PWMOutputDevice
 import RPi.GPIO as GPIO
 import time
 import sys
 
-#change what you need here.
-#comment/uncomment in main to debug.
-#don't mess around with other stuff! if you really-really need to, call or text me.
+# Configuration settings
 servoPWMPin = 13
 servoSpeed = 35
-        
 encoderInputPin = 6
 stepCountOnDisc = 8
-        
 delayBetweenStep = 0.01
-
 highToLow = True
 initialDrive = True
 plusStep = 0
-#plus step is supposed to be 0 if initial drive is true and 1 if initial drive is false
-#but I am not entirely sure, so it's there
-
 
 class Servo:
-    def __init__(self, pwmPin, speed,direction):
-        self.servo = PWMOutputDevice(pwmPin, frequency=50) 
-        self.speed = 90 + (speed*direction)
-        
+    def __init__(self, pwmPin, speed, direction):
+        self.servo = PWMOutputDevice(pwmPin, frequency=50)
+        self.speed = 90 + (speed * direction)
         self.servo.value = 0
 
     def testMotor(self, forSecond):
         self.servo.value = self._calculateDutyCycle(self.speed)
         time.sleep(forSecond)
-        self.servo.value = self._calculateDutyCycle(90)  
-        
+        self.servo.value = self._calculateDutyCycle(90)
+
     def driveMotor(self):
         self.servo.value = self._calculateDutyCycle(self.speed)
-    
+
     def stopMotor(self):
-        self.servo.value = 0  
+        self.servo.value = 0
 
     def _calculateDutyCycle(self, angle):
-
-        return (angle / 180) * (0.1) + 0.02
+        return (angle / 180) * 0.1 + 0.02
 
 class EncoderAndDisc:
-
     def __init__(self, inputPin, stepCountOnDisc):
         self.inputPin = inputPin
         self.stepCountOnDisc = stepCountOnDisc
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.inputPin, GPIO.IN,GPIO.PUD_DOWN)
-           
-    def setup(self):
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.inputPin, GPIO.IN,GPIO.PUD_DOWN)
-    
+        GPIO.setup(self.inputPin, GPIO.IN, GPIO.PUD_DOWN)
+
     def angleToStep(self, angle):
         return angle / (360 / self.stepCountOnDisc)
 
@@ -64,20 +48,19 @@ class EncoderAndDisc:
             pass
         while GPIO.input(self.inputPin) == 1:
             pass
-    
+
     def waitForLowToHigh(self):
         while GPIO.input(self.inputPin) == 1:
             pass
         while GPIO.input(self.inputPin) == 0:
             pass
-        
+
     def printSignal(self):
         while True:
-            print(GPIO.input(self.inputPin));
+            print(GPIO.input(self.inputPin))
 
 class System:
-    def __init__(self, delayBetweenStep, servo, encoderAndDisc, highToLow,initialDrive,plusStep):
-        
+    def __init__(self, delayBetweenStep, servo, encoderAndDisc, highToLow, initialDrive, plusStep):
         self.delayBetweenStep = delayBetweenStep
         self.servo = servo
         self.encoderAndDisc = encoderAndDisc
@@ -85,54 +68,48 @@ class System:
         self.initialDrive = initialDrive
         self.plusStep = plusStep
 
-
     def driveMotorUntilSignalHL(self):
         self.servo.driveMotor()
         self.encoderAndDisc.waitForHighToLow()
         self.servo.stopMotor()
-        
+
     def driveMotorUntilSignalLH(self):
         self.servo.driveMotor()
         self.encoderAndDisc.waitForLowToHigh()
-        self.servo.stopMotor()    
-        
+        self.servo.stopMotor()
+
     def goToAngle(self, angle):
         stepToTravel = self.encoderAndDisc.angleToStep(angle)
-        if initialDrive:
+        if self.initialDrive:
             self.servo.driveMotor()
-        
-        for i in range(int(stepToTravel)+self.plusStep):  
-            print("step: "+str(i))
+
+        for i in range(int(stepToTravel) + self.plusStep):
+            print(f"step: {i}")
             if self.highToLow:
                 self.driveMotorUntilSignalHL()
             else:
-                self.driveMotorUntilSignalLH();
-            
+                self.driveMotorUntilSignalLH()
+
             time.sleep(self.delayBetweenStep)
-        
+
         self.servo.stopMotor()
-        
-    def goFor(self,stepToTravel): 
-        
-        if initialDrive:
+
+    def goFor(self, stepToTravel):
+        if self.initialDrive:
             self.servo.driveMotor()
-        
-        for i in range(int(stepToTravel)+self.plusStep):  
-            print("step: "+str(i))
+
+        for i in range(int(stepToTravel) + self.plusStep):
+            print(f"step: {i}")
             if self.highToLow:
                 self.driveMotorUntilSignalHL()
             else:
-                self.driveMotorUntilSignalLH();
-            
+                self.driveMotorUntilSignalLH()
+
             time.sleep(self.delayBetweenStep)
-        
+
         self.servo.stopMotor()
-        
 
     def cleanup(self):
-        if hasattr(self, 'client_socket'):
-            self.client_socket.close()
-        self.socket.close()
         GPIO.cleanup()
         print("GPIO cleanup completed")
         exit()
@@ -146,55 +123,27 @@ def isInt(x):
 
 def warnAndExit():
     print("Stop trying to break the program.")
-    exit();
+    exit()
+
+if len(sys.argv) < 2 or not isInt(sys.argv[1]) or len(sys.argv) > 4:
+    warnAndExit()
+
+stepToTravel = int(sys.argv[1])
 
 direction = 1
+if len(sys.argv) >= 3:
+    direction = int(sys.argv[2])
+    if direction != 1 and direction != -1:
+        warnAndExit()
 
-numberOfArgs = len(sys.argv)
+if len(sys.argv) == 4:
+    initialDrive = int(sys.argv[3])
+    if initialDrive != 0 and initialDrive != 1:
+        warnAndExit()
+    initialDrive = bool(initialDrive)
 
-for i in range(1, numberOfArgs):
-    if isInt(sys.argv[i]):
-        sys.argv[i] = int(sys.argv[i])
-    else:
-        warnAndExit()  
-
-
-if numberOfArgs<2 or sys.argv[1]>25 or numberOfArgs>4:
-    warnAndExit();
-
-
-stepToTravel = sys.argv[1]
-
-if numberOfArgs==3:
-    sys.argv[2]==direction
-    
-elif numberOfArgs==4:
-    sys.argv[2]==direction
-    sys.argv[3]==initialDrive
-    
-    if (sys.argv[3]!=0 or sys.argv[3]!=1):
-        warnAndExit();
-    else:
-        if sys.argv[3]==0:
-            initialDrive=False
-        else:
-            initialDrive=True
-    
-
-if (direction!=1 or direction!=-1):
-    warnAndExit();
-
-
-servo = Servo(servoPWMPin,servoSpeed,direction)
-
+servo = Servo(servoPWMPin, servoSpeed, direction)
 encoderAndDisc = EncoderAndDisc(encoderInputPin, stepCountOnDisc)
-
-system = System(delayBetweenStep, servo, encoderAndDisc
-                ,highToLow,initialDrive,plusStep)
+system = System(delayBetweenStep, servo, encoderAndDisc, highToLow, initialDrive, plusStep)
 
 system.goFor(stepToTravel)
-
-#orderList = ['6','G','4','B'];
-#system.filterProcedure(orderList);
-
-#system.encoderAndDisc.printSignal();
