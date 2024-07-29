fbb920617d114288b2548a88379faa97f52b4682
Sokisati <yagizalpersoy@gmail.com>
2024-07-29 19:33:23 +0300

    bug fix

--- readme.py
+++ readme.py
@@ -1,7 +1,7 @@
 """
-move_step programýnda 1. argüman kaç adým atlayacaðýný yazacaksýnýz.
+move_step programýnda 1. arguman kac adým atlayacagýný yazacaksýnýz.
 
-2 ve 3. argüman opsiyonel. 2'ye -1 yazarsanýz ters, 1 yazarsanýz düz yönde gider. 3'e 1 yazarsanýz
-motor ilk sinyale (sinyal, yükselen veya alçalan kenar) ihtiyaç duymadan gider, 0 yazarsanýz ilk sinyali
+2 ve 3. arguman opsiyonel. 2'ye -1 yazarsanýz ters, 1 yazarsanýz duz yönde gider. 3'e 1 yazarsanýz
+motor ilk sinyale (sinyal, yukselen veya alcalan kenar) ihtiyac duymadan gider, 0 yazarsanýz ilk sinyali
 alana kadar hareket etmez.
 """
