f488b226edf78b75d52af1512c107d8bcccbc3d3
Sokisati <yagizalpersoy@gmail.com>
2024-07-30 15:07:34 +0300

    readme update

--- readme.py
+++ readme.py
@@ -1,6 +1,8 @@
 """
 move_step programinda 1. arguman kac adim atlayacagini yazacaksiniz.
 
+parametre degistirmek istiyorsaniz module_file'a gidin.
+
 2 ve 3. arguman opsiyonel. 2'ye -1 yazarsaniz ters, 1 yazarsaniz duz yonde gider. 3'e 1 yazarsaniz
 motor ilk sinyale (sinyal, yukselen veya alcalan kenar) ihtiyac duymadan gider, 0 yazarsaniz ilk sinyali
 alana kadar hareket etmez.
