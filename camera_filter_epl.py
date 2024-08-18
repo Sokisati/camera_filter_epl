426e881896dc39716357d057ba59da9001e79727
yagizalpersoy@gmail.com <yagizalpersoy@gmail.com>
2024-08-19 00:23:05 +0300

    fix

--- camera_filter_epl.py
+++ camera_filter_epl.py
@@ -7,7 +7,7 @@ encoderAndDisc = EncoderAndDisc(encoderInputPin, stepCountOnDisc,delayForSignal)
 system = System(delayBetweenStep, servo, encoderAndDisc,
                 portToListen,highToLow,initialDrive,plusStep,revertLastSignal)
 
-system.mainLoop();
+#system.mainLoop();
 system.servo.stopMotor()
 #orderList = ['6','G','4','B'];
 #system.filterProcedure(orderList);
